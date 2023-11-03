1414e24ccf9aeae8a631cc2def72e1dc6525c95b
EmilieViot <e.larvol@outlook.fr>
2023-11-03 14:02:39 +0000

    adding changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,17 +20,26 @@ function loadPalette(palette)
 {
     let colors = palette;
     
-    console.log(colors)
-    
     // le code de l'étape 1 se passe ici
+    
+    let divHeader = document.querySelectorAll("body > header > div")
+    
+    for (let i=0;i<divHeader.length;i++)
+    
+    {divHeader[i].style.backgroundColor = colors[i]}
 }
-
 
 window.addEventListener("DOMContentLoaded", function(){
     loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
 
     // le code de l'étape 2 se passe ici
+    
+let divHeader = document.querySelectorAll("body header div");
 
+divHeader.addEventListener("click", selectColor);
+{for (let i=0;i<divHeader.length;i++)
+    {console.log(getSelectedColor(i));}
+}
 
     // le code de l'étape 3 se passe ici
 
